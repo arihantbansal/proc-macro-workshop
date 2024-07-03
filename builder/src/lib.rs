28a3ab30eceb2b118d6fb4f7586d10261b081189
arihantbansal <17180950+arihantbansal@users.noreply.github.com>
2024-07-03 15:23:39 +0530

    call setters

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,5 +1,5 @@
 use proc_macro::TokenStream;
-use proc_macro2::{Ident, Span};
+use proc_macro2::Ident;
 use quote::quote;
 use syn::{parse_macro_input, DeriveInput};
 
@@ -9,16 +9,9 @@ pub fn derive(input: TokenStream) -> TokenStream {
 
     let name = &input.ident;
 
-    let name_builder = Ident::new(&format!("{}Builder", name), Span::call_site());
+    let name_builder = Ident::new(&format!("{}Builder", name), name.span());
 
     let expanded = quote! {
-        pub struct #name_builder {
-            executable: Option<String>,
-            args: Option<Vec<String>>,
-            env: Option<Vec<String>>,
-            current_dir: Option<String>,
-        }
-
         impl #name {
             pub fn builder() -> #name_builder {
                 #name_builder {
@@ -27,6 +20,35 @@ pub fn derive(input: TokenStream) -> TokenStream {
                     env: None,
                     current_dir: None,
                 }
+            }
+        }
+
+        pub struct #name_builder {
+            executable: Option<String>,
+            args: Option<Vec<String>>,
+            env: Option<Vec<String>>,
+            current_dir: Option<String>,
+        }
+
+        impl #name_builder {
+            pub fn executable(&mut self, executable: String) -> &mut Self {
+                self.executable = Some(executable);
+                self
+            }
+
+            pub fn args(&mut self, args: Vec<String>) -> &mut Self {
+                self.args = Some(args);
+                self
+            }
+
+            pub fn env(&mut self, env: Vec<String>) -> &mut Self {
+                self.env = Some(env);
+                self
+            }
+
+            pub fn current_dir(&mut self, current_dir: String) -> &mut Self {
+                self.current_dir = Some(current_dir);
+                self
             }
         }
     };
